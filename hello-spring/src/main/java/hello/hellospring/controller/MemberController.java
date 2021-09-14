805fdb394701d4dfd034e375ff99dead4f0619c1
ktr83385 <ktr83385@gmail.com>
2021-09-14 14:28:12 +0900

    MemberController 생성자 작성, MemberService @Service 주석 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,17 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired  // 멤버 컨트롤러를 스프링 컨테이너가 생성해주니까, 생성자 호출됨.
+    //이때 Autowired 주석이 되어 있으면, 멤버 서비스를 스프링이 스프링 컨테이너에서 가져와서 연결시켜줌.
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
 }
