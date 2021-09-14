404f1196f5c4f96d1f3714ec44cf09dc38418ce3
ktr83385 <ktr83385@gmail.com>
2021-09-14 14:43:50 +0900

    MemberController 생성자 작성, MemberService @Service 주석 추가

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();   // 동시성 문제를 일단은 고려 안하고 해쉬맵을 쓰겠다.
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,6 +2,7 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {   // 직접 new 키워드로 생성 X
         // 외부에서 넣어준다. -> Dependence Injection (DI) 라고 한다.
         // 왜 이렇게 하냐면, 테스트 코드 실행할 때 같은 인스턴스를 테스트할 수 있도록 해준다. (그때 그때 생성 X)
