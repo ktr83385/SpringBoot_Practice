2fbb0b2de6080fcba00e6de70656d5279728972a
ktr83385 <ktr83385@gmail.com>
2021-09-30 10:04:25 +0900

    컨트롤러 테스트

--- insta/src/main/java/com/practice/prac/controller/MpaUsrHomeController.java
+++ insta/src/main/java/com/practice/prac/controller/MpaUsrHomeController.java
@@ -1,5 +1,27 @@
 package com.practice.prac.controller;
 
-public class MpaUsrHomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+public class MpaUsrHomeController {
+    @RequestMapping("/mpaUsr/home/main")
+    @ResponseBody
+    public String showMain() {
+        return "안녕";
+    }
+    
+    @RequestMapping("/mpaUsr/home/main2")
+    @ResponseBody
+    public String showMain2() {
+        return "잘가";
+    }
+
+    @RequestMapping("/mpaUsr/home/main3")
+    // http://localhost:8024/mpaUsr/home/main3?a=10&b=30 으로, 주소 뒤에 물음표와 함께 값 입력
+    @ResponseBody
+    public int showMain3(int a, int b) {
+        return a + b;
+    }
 }
