cc9d63863aea1ebcd2db3942af222bafdf28886b
ktr83385 <ktr83385@gmail.com>
2021-09-30 11:23:09 +0900

    게시물 보관했다가 요청 시 반환

--- insta/src/main/java/com/practice/prac/controller/MpaUsrArticleController.java
+++ insta/src/main/java/com/practice/prac/controller/MpaUsrArticleController.java
@@ -7,11 +7,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MpaUsrArticleController {
-    private int articleLastId = 0;
+    private List<Article> articles;
+    private int articleLastId;
 
-//    @RequestMapping("/mpaUsr/article/doWrite")
+    public MpaUsrArticleController() {
+        articles = new ArrayList<>();
+        articleLastId = 0;
+    }
+
+    //    @RequestMapping("/mpaUsr/article/doWrite")
 //    @ResponseBody
 //    public String doWrite(String title, String body) {
 //        int id = articleLastId + 1;
@@ -28,15 +37,43 @@ public class MpaUsrArticleController {
     @RequestMapping("/mpaUsr/article/doWrite")
     @ResponseBody
     public ResultData doWrite(String title, String body) {
+        int id = writeArticle(title, body);
+        Article article = getArticleById(id);
+
+        return new ResultData("S-1", id + "번 글이 등록되었습니다.", article);
+    }
+
+    private int writeArticle(String title, String body) {
         int id = articleLastId + 1;
         String regDate = Util.getNowDateStr();
         String updateDate = Util.getNowDateStr();
 
-        articleLastId = id;
-
         Article article = new Article(id, regDate, updateDate, title, body);
 
-        return new ResultData("S-1", id + "번 글이 등록되었습니다.", article);
+        articleLastId = id;
+        articles.add(article);
+
+        return id;
+    }
+
+    @RequestMapping("/mpaUsr/article/getArticle")
+    @ResponseBody
+    public ResultData getArticle(int id) {
+        Article article = getArticleById(id);
+        if (article == null) {
+            return new ResultData("F-1", id + "번 글은 존재하지 않습니다.", article);
+        }
+
+        return new ResultData("S-1", article.getId() + "번 글 입니다.", article);
+    }
+
+    private Article getArticleById(int id) {
+        if (id > articles.size()) {
+            return null;
+        }
+        Article article = articles.get(id-1);
+
+        return article;
     }
 }
 
